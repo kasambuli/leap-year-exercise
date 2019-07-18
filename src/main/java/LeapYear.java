c2d0daf88614b790f72f50f2413eb66f5c7f5ec5
kasambuli <cynthiakasambuli@gmail.com>
2019-07-18 12:15:07 +0300

    Added tests for numbers divisible by 4 and divisible by 100(but not divisible by 400)

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+    public boolean isLeapYear(int year){
+        if (year % 100 == 0){
+            return false;
+        }
+        else{
+            return year % 4 == 0;
+        }
+
+    }
+}
---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeapYearTest {
+    @Test
+    public void isLeapYear_forNumberDivisibleByFour_true(){
+        LeapYear leapYear = new LeapYear();
+        assertEquals(true, leapYear.isLeapYear(2012));
+    }
+
+    @Test
+    public void isLeapYear_forNumbersNotDivisibleByFour_false() {
+        LeapYear leapYear = new LeapYear();
+        assertEquals(false, leapYear.isLeapYear(1999));
+    }
+
+    @Test
+    public void isLeapYear_forMultiplesOfOneHundred_false() {
+        LeapYear leapYear = new LeapYear();
+        assertEquals(false, leapYear.isLeapYear(1900));
+    }
+}
